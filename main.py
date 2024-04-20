643dd85a01d7ddae15565035acc235e97cfd408b
Cocurie_tsuruta <cocrie.test@gmail.com>
2024-04-20 20:50:04 +0900

    second commit

--- main.py
+++ main.py
@@ -1,84 +1,85 @@
-import json
-import openpyxl
-import glob
-
-import cv2 #画像表示デバック用
-
+import json#jsonファイル作成
+import openpyxl#excelファイル連携
+import glob #指定フォルダから画像抽出
+import cv2 #画像表示(現時点ではデバック用)
 
 
+##################################################
+############　　　　以下、使い方　　　　###############
+##################################################
 
-#画像を表示（デバック用）
-#image = cv2.imread('/Users/tsurutashuichi/PycharmProjects/pythonProject2/image_strage/dog.犬.1-1.jpg')
-#cv2.imshow('image_test',image)
-#cv2.waitKey(0)
-#cv2.destroyWindow('image_test')
-#print(cv2.__version__)
+#【手順１】取り込むexcelファイルを「main.py」と同じディレクトリに入れて、以下でファイル名を指定（''の中を変更）
+excelFile = 'forTest.xlsx'
 
-
-#imageファイルをフォルダから抽出
-imageFileName = "dog.犬.1-1.jpg"
-imagePath = glob.glob("/Users/tsurutashuichi/PycharmProjects/pythonProject2/image_strage/"+ imageFileName)
-print(imageFileName)
-print(imagePath)
+#【手順２】取り込むexcelファイルの初めの行と終わりの行を指定
+startRow = 3
+endRow = 5
 
 
 
-#JSONファイルを作成
-tuningSet = {
-    'prompt':'竣工年次1972年の単純PCプレテンT桁橋の主桁の写真です。この橋は精密な点検を必要としますか？',
-
-    'additionalInfo':'主桁の下フランジ側面に鉄筋露出(150×200×20mm)が見られる',
-
-    'answer':'この橋梁は精密検査を必要としていません。この損傷は、縦目地からの路面水の影響により、かぶり不足の鉄筋が腐食・膨張 し、露出したと推定されます。前回の点検に記録はなく、縦目地からの漏水が継続するため、損傷が進行する可能性は高いが、局所的な鉄筋露出であることから直ちに部材の耐荷力が低下する状況ではない。'
-}
-out = json.dumps(tuningSet, indent=2, ensure_ascii=False)
-with open('sample.json', 'wt', encoding='utf-8') as fout:
-    fout.write(out)
+##################################################
+#############　　　　以下、本体　　　　################
+##################################################
 
 
 #excelファイルをオープン
-wb = openpyxl.load_workbook('forTest.xlsx')
+wb = openpyxl.load_workbook(excelFile)
 #シートを選択
 sheet = wb['Sheet1']
-#セルの値を取得
-for i in range(1,8):
-    prompt = sheet.cell(row=i, column=1).value \
-                +'と' \
-                    + sheet.cell(row=i, column=2).value
-    print(prompt)
 
-#新たに追加するデータ
-add_data = {
-    'ddd':{
-        'e':'eee'
+
+
+#対象エクセルの行数分だけ繰り返し
+count = startRow
+while count < endRow +1:
+
+#imageファイルをフォルダから抽出
+    imageFromExcel = sheet.cell(row=count, column=2).value
+    img = glob.glob('/Users/tsurutashuichi/PycharmProjects/pythonProject2/image_strage/'+imageFromExcel)
+
+#promptを生成（最後の一文は「この犬をペットにしたいですか？」）
+    margeCell = sheet.cell(row=count, column=3).value\
+                +'で'+sheet.cell(row=count, column=4).value\
+                    +'出身の犬です。'\
+                        +'この犬をペットにしたいですか？'
+
+#JSONファイルを作成
+    tuningSet = {
+        sheet.cell(row=count, column=1).value:{
+        'image':img,
+        'prompt':margeCell,
+        'additionalInfo':{'fixedInfo':"(犬の場合は情報なし)",
+                          'not_fixedInfo':{
+                              'item1':sheet.cell(row=count, column=5).value,
+                              'item2':sheet.cell(row=count, column=6).value}},
+        'answer':sheet.cell(row=count, column=7).value,
+        }
     }
-}
-#JSONファイルに追加する辞書データをupdateで追加する
-with open('sample.json','r')as file:
-    data = json.load(file)
-    data.update(add_data)
 
-#JSONファイルにデータを追加(write)する
-with open('sample.json', 'w')as file:
-    json.dump(data, file, indent=2, ensure_ascii=False)
+    out = json.dumps(tuningSet, indent=4, ensure_ascii=False)
+    with open('sample.json', 'a', encoding='utf-8') as fout:
+        fout.write(out)
+
+    count += 1
 
 
-
+else:
+    print('☆☆☆☆☆jsonファイル出力完了!おめでとう！！☆☆☆☆☆')
 
 
 
 
+##################################################
+#############　　　　以下、材料　　　　################
+##################################################
 
-
-#####################
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+#def print_hi(name):
+#    # Use a breakpoint in the code line below to debug your script.
+#    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
+#if __name__ == '__main__':
+#    print_hi('PyCharm')
 
 
 #testSet = {
@@ -93,7 +94,32 @@ if __name__ == '__main__':
 #    }
 #}
 
+#画像を表示（デバック用）
+#image = cv2.imread('/Users/tsurutashuichi/PycharmProjects/pythonProject2/image_strage/dog.犬.1-1.jpg')
+#cv2.imshow('image_test',image)
+#cv2.waitKey(0)
+#cv2.destroyWindow('image_test')
+#print(cv2.__version__)
+
+#セルの値を取得
+#for i in range(2,8):
+#    prompt = sheet.cell(row=i, column=1).value \
+#                +'と' \
+#                    + sheet.cell(row=i, column=2).value
+#    print(prompt)
+
+#JSONファイルに追加する辞書データをupdateで追加する
+#    with open('sample.json','r')as file:
+#        data = json.load(file)
+#        data.update(add_data)
+
+#JSONファイルにデータを追加(write)する
+#    with open('sample.json', 'a')as file:
+#        json.dump(data, file, indent=2, ensure_ascii=False)
 
 
-
-
+#imageファイルをフォルダから抽出
+#imageFileName = "dog.犬.1-1.jpg"
+#imagePath = glob.glob("/Users/tsurutashuichi/PycharmProjects/pythonProject2/image_strage/"+ imageFileName)
+#print(imageFileName)
+#print(imagePath)
